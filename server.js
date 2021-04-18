583c9c81bfaa8817ddee9ac9bdc15f72f22d8630
Jonathan <cuajonathan279@gmail.com>
2021-04-18 17:53:02 -0600

    ci: Installing babel to work with EcmaScript 6

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
