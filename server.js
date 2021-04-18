f17bbb78080d4c233345e44c7e38598db994914d
Jonathan <cuajonathan279@gmail.com>
2021-04-18 17:37:37 -0600

    ci: adding Cors and Morgan middleware to Express

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.listen(app.get('port'), () => {
-  console.log("Server on: ", `http://localhost:${app.get('port')}`);
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+  console.log("Server on: ", `http://localhost:${app.get("port")}`);
 });
