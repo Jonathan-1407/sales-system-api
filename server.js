d0b191ba73db4679929103ea7116e5c17b45b5ec
Jonathan <cuajonathan279@gmail.com>
2021-04-18 18:20:46 -0600

    ci: Showing the home page: index.html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -8,6 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("port", process.env.PORT || 3000);
 
