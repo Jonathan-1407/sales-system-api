939b6c2a3d3c14a067f3b2833d2027a49a62d78f
Jonathan <cuajonathan279@gmail.com>
2021-04-18 19:56:45 -0600

    ci: Creating the Category model

---
+++ models/Category/category.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  name: { type: String, maxlength: 50, unique: true, required: true },
+  description: { type: String, maxlength: 255},
+  state: { type: Number, default: Date.now}
+});
+
+const Category = mongoose.model("category", categorySchema);
+
+export default Category;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+import Category from "./Category/category";
+
+export default {
+  Category
+};
