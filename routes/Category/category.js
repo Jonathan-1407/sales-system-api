684fb101bda31a9bcf1b76fae60933256e5a9b5c
Jonathan <cuajonathan279@gmail.com>
2021-04-21 22:28:04 -0600

    ci: Allowing Category routes to the admin and grocer user

--- routes/Category/category.js
+++ routes/Category/category.js
@@ -1,14 +1,15 @@
 import routerx from "express-promise-router";
 import categoryController from "../../controllers/Category/category";
+import auth from "../../middlewares/Auth/authenticate";
 
 const router = routerx();
 
-router.post("/add", categoryController.add);
-router.get("/query", categoryController.query);
-router.get("/list", categoryController.list);
-router.put("/update", categoryController.update);
-router.delete("/remove", categoryController.remove);
-router.put("/enable", categoryController.enable);
-router.put("/disable", categoryController.disable);
+router.post("/add", auth.verifyGrocer, categoryController.add);
+router.get("/query", auth.verifyGrocer, categoryController.query);
+router.get("/list", auth.verifyGrocer, categoryController.list);
+router.put("/update", auth.verifyGrocer, categoryController.update);
+router.delete("/remove", auth.verifyGrocer, categoryController.remove);
+router.put("/enable", auth.verifyGrocer, categoryController.enable);
+router.put("/disable", auth.verifyGrocer, categoryController.disable);
 
 export default router;
