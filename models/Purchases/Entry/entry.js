25abae3bf22485f99520db126749bf543d28a208
Jonathan <cuajonathan279@gmail.com>
2021-04-22 23:46:36 -0600

    ci: Purchases - Embedded Entry Model

---
+++ models/Purchases/Entry/entry.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const entrySchema = new Schema({
+  user: { type: Schema.ObjectId, ref: "users", required: true },
+  person: { type: Schema.ObjectId, ref: "person", required: true },
+  voucher_type: { type: String, maxlength: 20, required: true },
+  voucher_series: { type: String, maxlength: 7 },
+  voucher_number: { type: String, maxlength: 10, required: true },
+  tax: { type: Number, required: true },
+  total: { type: Number, required: true },
+  detalles: [
+    {
+      _id: {
+        type: String,
+        required: true
+      },
+      article: {
+        type: String,
+        required: true
+      },
+      amount: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  state: { type: Number, default: 1 },
+  created_at: { type: Date, default: Date.now }
+});
+
+const Entry = mongoose.model("entry", entrySchema);
+
+export default Entry;
--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import User from "./User/user";
 import Person from "./Person/person";
 import Category from "./Category/category";
 import Article from "./Article/article";
+import Entry from "./Purchases/Entry/entry";
 
 export default {
   User,
   Person,
   Category,
-  Article
+  Article,
+  Entry
 };
