3b74bb062b251c1dba8333898ba7881191e7616f
Jonathan <cuajonathan279@gmail.com>
2021-04-24 21:35:45 -0600

    ci: Sales - Embedded Sale Model

---
+++ models/Sales/Sale/sale.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const saleSchema = new Schema({
+  user: { type: Schema.ObjectId, ref: "users", required: true },
+  person: { type: Schema.ObjectId, ref: "person", required: true },
+  voucher_type: { type: String, maxlength: 20, required: true },
+  voucher_series: { type: String, maxlength: 7 },
+  voucher_number: { type: String, maxlength: 10, required: true },
+  tax: { type: Number, required: true },
+  total: { type: Number, required: true },
+  detalles: [
+    {
+      _id: {
+        type: String,
+        required: true
+      },
+      article: {
+        type: String,
+        required: true
+      },
+      amount: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      discount: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  state: { type: Number, default: 1 },
+  created_at: { type: Date, default: Date.now }
+});
+
+const Sale = mongoose.model("sale", saleSchema);
+
+export default Sale;
--- models/index.js
+++ models/index.js
@@ -3,11 +3,13 @@ import Person from "./Person/person";
 import Category from "./Category/category";
 import Article from "./Article/article";
 import Entry from "./Purchases/Entry/entry";
+import Sale from "./Sales/Sale/sale";
 
 export default {
   User,
   Person,
   Category,
   Article,
-  Entry
+  Entry,
+  Sale
 };
