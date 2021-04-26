d76d3fdd5b033eaf2aaff99e308d3451751eef2c
Jonathan <cuajonathan279@gmail.com>
2021-04-25 22:54:15 -0600

    refactor: Renaming the variable entrySchema by entryController in the path file entry.js

--- routes/Purchases/Entry/entry.js
+++ routes/Purchases/Entry/entry.js
@@ -1,15 +1,15 @@
 import routerx from "express-promise-router";
-import entrySchema from "../../../controllers/Purchases/Entry/entry";
+import entryController from "../../../controllers/Purchases/Entry/entry";
 import auth from "../../../middlewares/Auth/authenticate";
 
 const router = routerx();
 
-router.post("/add", auth.verifyGrocer, entrySchema.add);
-router.get("/query", auth.verifyGrocer, entrySchema.query);
-router.get("/list", auth.verifyGrocer, entrySchema.list);
-router.get("/last-year-chart", auth.verifyUser, entrySchema.last_year_chart);
-router.post("/search-between-dates", auth.verifyUser, entrySchema.search_between_dates);
-router.put("/approve", auth.verifyGrocer, entrySchema.approve);
-router.put("/cancel", auth.verifyGrocer, entrySchema.cancel);
+router.post("/add", auth.verifyGrocer, entryController.add);
+router.get("/query", auth.verifyGrocer, entryController.query);
+router.get("/list", auth.verifyGrocer, entryController.list);
+router.get("/last-year-chart", auth.verifyUser, entryController.last_year_chart);
+router.post("/search-between-dates", auth.verifyUser, entryController.search_between_dates);
+router.put("/approve", auth.verifyGrocer, entryController.approve);
+router.put("/cancel", auth.verifyGrocer, entryController.cancel);
 
 export default router;
